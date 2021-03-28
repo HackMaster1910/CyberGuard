58c37fdb196877a956f460ce6ad47c9ff910d38c
HackMaster1910 <80475386+HackMaster1910@users.noreply.github.com>
2021-03-28 15:25:40 +0100

    Nicer bug embeds.

--- cogs/bug.py
+++ cogs/bug.py
@@ -11,11 +11,15 @@ class BugCommand(commands.Cog):
             if issue==None:
                 await ctx.channel.send('You have to enter a bug to report! For example: ;bug "<bug>"')
             else:
-                embed=discord.Embed(Color=0xFF0000)
+                embed=discord.Embed(color=0xFF0000)
                 embed.add_field(name="Bug Report! 🐛", value=f'Your bug: "{issue}" has been successfully sent!')
                 await ctx.channel.send(embed=embed)
+                embed=discord.Embed(color=0xFF0000)
+                embed.add_field(name="New Bug Report! 🐛", value=f'New bug: "{issue}"')
+                embed.set_author(name=ctx.author)
+                embed.set_thumbnail(url=ctx.author.avatar_url)
                 bugchannel = self.client.get_channel(825713979304968202)
-                await bugchannel.send(f'{ctx.author} just sent the bug: "{issue}"')
+                await bugchannel.send(embed=embed)
 
 
 
